5cc911bd6eb442151b2982bd87e6c3bcdeb47911
17-100 <sebisteri@outlook.com>
2021-10-11 16:49:50 +0200

    first commit

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function Contact() {
+    const email = "sebisteri@outlook.com"
+  return (<>
+    <div>
+        <h1>Oezge Sebisteri</h1>
+        <h2>sebisteri@outlook.com</h2>
+    </div>
+  </>)
+}
